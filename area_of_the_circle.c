c796077fc58612c075d2d65009bcb97244e729eb
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-04-22 19:23:06 +0530

    commit

--- area_of_the_circle.c
+++ area_of_the_circle.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 #define pi 3.14;
 int main(){
-    float radius,area,circumference;
+    float radius,area;
     printf("Enter the radius of the circle :");
     scanf("%f",&radius);
 area=radius*radius*pi;
 printf("\n Area of the radius is : %f",area);
-    circumference=radius*2*pi;
-    printf("\n circumference of the circle is %f:",circumference);
+    
 return 0;
 }
