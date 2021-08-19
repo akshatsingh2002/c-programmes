f2cc1c761817a2093adb95b932b1eb0fef3cf8f8
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-08-19 11:01:04 +0530

    cimmit

---
+++ w4p1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter the first number\n");
+    scanf("%d",&a);
+     printf("Enter the second number\n");
+    scanf("%d",&b);
+     printf("Enter the third number\n");
+    scanf("%d",&c);
+    if(a<b&&a<c)
+    printf("\n %d is the smallest",a);
+    else if(b<a&&b<c)
+    printf("\n %d is the smallest",b);
+    else
+     printf("\n %d is the smallest",c);
+    return 0;
+}
