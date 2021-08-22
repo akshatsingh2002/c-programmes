ab74bca395c82931fdb047cc17418e215aa8d73f
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2021-08-22 07:56:16 +0530

    commit

--- reversearray.c
+++ reversearray.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int i,j,n,arr[10],ar[10];
+    int i,n,arr[10],search,count=0,c;
     printf("Enter the number of elemntes you want in the array\n");
     scanf("%d",&n);
     for(i=0;i<n;i++)
@@ -14,12 +14,19 @@ int main(){
     printf("\n%d",arr[i]);
     
     }
-for(i=0,j=n-1;j>=0;j--,i++){
-    ar[j]=arr[i];
-}
-for(i=0,j=n-1;j>=0;j--,i++){
-    printf("%d\n",ar[i]);
-}
+
+    printf("Enter the number to check whether it is available in the list of not\n");
+    scanf("%d",&search);
+    for(c=0;c<n;c++){
+        if(arr[c]==search)
+        {
+        printf("%d is present on location%d",search, c+1);
+        count++;
+        }
+
+    }
+    printf("%d is number present %d times in the list",search,count);
+
 
     return 0;
 }
